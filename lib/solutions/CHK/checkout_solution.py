fa6dcd97dd4e29e91ed1b0ab9834d93baf557934
sbx_user1051 <sbx_user1051@169.254.6.101>
2022-07-03 13:00:35 +0000

    Sun Jul 03 13:00:35 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,15 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+def parse_request(skus):
+    item_list = []
+    if not isinstance(skus, str):
+        return None
+
+
+
 def checkout(skus):
+    item_list = parse_request(skus)
     raise NotImplementedError()
+
+
