4545f090d564d69b818421c084595def8b038c9a
sbx_user1051 <sbx_user1051@169.254.6.101>
2022-07-03 13:24:35 +0000

    Sun Jul 03 13:24:35 UTC 2022

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -16,3 +16,21 @@ def test_parse_request_given_non_existing_item_identifier():
     obtained_items = checkout_solution.parse_request(request)
 
     assert obtained_items is None
+
+
+def test_parse_request_given_request_without_letter():
+    request = "AAB,"
+
+    obtained_items = checkout_solution.parse_request(request)
+
+    assert obtained_items is None
+
+
+def test_parse_request_given_request_with_mixed_case():
+    request = "AaBC"
+
+    obtained_items = checkout_solution.parse_request(request)
+
+    expected_items = {"A": 2, "B": 1, "C": 1, "D": 0}
+    assert obtained_items == expected_items
+
