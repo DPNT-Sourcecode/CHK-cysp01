7cb04cf046f7f03ced3e8013e1e6ef70a49f6aca
sbx_user1051 <sbx_user1051@169.254.6.101>
2022-07-03 13:12:35 +0000

    Sun Jul 03 13:12:35 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,18 +4,21 @@ ITEM_IDENTIFIERS = {"A", "B", "C", "D"}
 # noinspection PyUnusedLocal
 # skus = unicode string
 def parse_request(skus):
-    item_list = []
+    item_list = dict.fromkeys(ITEM_IDENTIFIERS, 0)
     if not isinstance(skus, str):
         return None
     for s in skus:
-        if s.upper() not in ITEM_IDENTIFIERS:
+        if s.upper() not in item_list.keys():
             return None
-
+        else:
+            item_list[s.upper()] = item_list[s.upper()] + 1
+    return item_list
 
 
 def checkout(skus):
     item_list = parse_request(skus)
     raise NotImplementedError()
+
 
 
 
